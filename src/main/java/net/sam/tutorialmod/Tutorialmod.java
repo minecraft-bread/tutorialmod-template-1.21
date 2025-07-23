c12d68cbb1ab5aa23e46d57a27226f9a033b2c34
minecraft-bread <sj42069123@gmail.com>
2025-07-23 04:56:50 -0700

    added pink garnet block and raw pink garnet block

--- src/main/java/net/sam/tutorialmod/Tutorialmod.java
+++ src/main/java/net/sam/tutorialmod/Tutorialmod.java
@@ -2,6 +2,7 @@ package net.sam.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sam.tutorialmod.block.ModBlocks;
 import net.sam.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class Tutorialmod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
